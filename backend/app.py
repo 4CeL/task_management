29dbbd38685c59926d2b9322069d5668bf7e2a78
Stefanus Marcellino <stefanusmarcellino18@gmail.com>
2026-09-13 19:03:14 +0700

    Perubhaan api 4

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(
     app,
     origins=[
-        "https://task-workflow-app.vercel.app/",
+        "https://task-workflow-app.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173"
     ],
